5694d577b9d98f4dd338f497eb0698eba99a5187
Sven Weintuch <sven@weintuch.net>
2014-02-16 20:25:48 +0100

    fixed typo

--- example.php
+++ example.php
@@ -8,8 +8,8 @@ include('phyph.php');
 $phyph = new Phyph();
 $phyph->loadPattern('pattern/phyph.en.php');
 
-// optionally you can also adjust a few params after the pattern was loaded
-$phyph->wordmin = 10; // words shorter than this value, will be ignored
+// optionally you can now adjust a few params
+$phyph->wordmin = 10; // words shorter than this value will be ignored
 
-// now just output some text, which will be hyphenated
+// now just print some hyphenated text
 echo $phyph->hyphenate('Abbreviation - or some other long word.');
