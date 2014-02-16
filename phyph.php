043f1fa2a8d51b8337aebf6da9aa82a87d2510b4
Sven Weintuch <sven@weintuch.net>
2014-02-16 20:19:07 +0100

    added license information & some comments

--- phyph.php
+++ phyph.php
@@ -1,18 +1,48 @@
 <?php
 
+/**
+ * Class Phyph
+ *
+ * A simple hyphenation class.
+ *
+ * Licensed under Creative Commons Attribution-ShareAlike 3.0
+ * http://creativecommons.org/licenses/by-sa/3.0/
+ *
+ * Phyph is a fork of phpHyphenator (http://yellowgreen.de/phphyphenator/)
+ */
 class Phyph
 {
 
+    /**
+     * these characters that indicate the end/beginning of a word
+     * @var string
+     */
     public $boundaries = "<>\t\n\r\0\x0B !\"§$%&/()=?….,;:-–_„”«»‘’'/\\‹›()[]{}*+´`^|©℗®™℠¹²³";
+    /**
+     * string which is inserted as hyphen
+     * @var string
+     */
     public $hyphen = '&shy;';
+    /**
+     * minimum length of word, which will be hyphenated
+     * @var int
+     */
     public $wordmin = 8;
-    public $leftmin;
-    public $rightmin;
-    public $charmin;
-    public $charmax;
+
+    private $leftmin;
+    private $rightmin;
+    private $charmin;
+    private $charmax;
 
     private $pattern = false;
 
+    /**
+     * This method tries to load the provided pattern-file.
+     * It returns FALSE in case of an error, otherwise nothing.
+     *
+     * @param bool $file
+     * @return bool
+     */
     public function loadPattern($file = false)
     {
         if (empty($file) || !file_exists($file) || $this->pattern) {
@@ -21,10 +51,12 @@ class Phyph
 
         $pattern = include("$file");
 
+        // ensure that we even loaded a pattern-file with a pattern
         if (empty($pattern['pattern'])) {
             return false;
         }
 
+        // if different options were provided, use them, otherwise some default values
         $this->leftmin  = isset($pattern['leftmin'])    ? $pattern['leftmin']   : 2;
         $this->rightmin = isset($pattern['rightmin'])   ? $pattern['rightmin']  : 2;
         $this->charmin  = isset($pattern['charmin'])    ? $pattern['charmin']   : 2;
@@ -33,6 +65,11 @@ class Phyph
         $this->parsePattern($pattern['pattern']);
     }
 
+    /**
+     * This method builds the internal pattern-array
+     *
+     * @param $pattern
+     */
     private function parsePattern($pattern)
     {
         $this->pattern = array();
@@ -47,16 +84,26 @@ class Phyph
         }
     }
 
+    /**
+     * This method hyphenates the given text. It will be split up into words,
+     * which then are hyphenated separately.
+     *
+     * @param $text
+     * @return string
+     */
     public function hyphenate($text)
     {
+        // check if we already have loaded a pattern, otherwise just return the text
         if (!$this->pattern) {
             return $text;
         }
 
+        // prepare some variables
         $text .= ' ';
         $return = array();
         $word = '';
 
+        // go through the text and hyphenate single words
         for ($i = 0, $len = mb_strlen($text); $i < $len; $i++) {
             $char = mb_substr($text, $i, 1);
             if (mb_strpos($this->boundaries, $char) === false) {
@@ -73,15 +120,24 @@ class Phyph
         return implode('', array_slice($return, 0, -1));
     }
 
+    /**
+     * This method returns the hyphenated string of the provided word.
+     *
+     * @param $word
+     * @return string
+     */
     private function hyphenateWord($word)
     {
+        // is the word long enough?
         if (mb_strlen($word) < $this->wordmin) {
             return $word;
         }
+        // does the word already contain the hyphen?
         if (mb_strpos($word, $this->hyphen) !== false) {
             return $word;
         }
 
+        // prepare some variables
         $matchWord = '.' . $word . '.';
         $matchWordLength = mb_strlen($matchWord);
 
@@ -94,6 +150,7 @@ class Phyph
 
         $hyphenateWord = array();
 
+        // walk through the word and figure out, where to insert the hyphens
         for ($position = 0; $position < ($matchWordLength - $this->charmin); $position++) {
             $maxPatternLength = min($this->charmax, $matchWordLength - $position);
 
@@ -120,6 +177,7 @@ class Phyph
             }
         }
 
+        // insert the hyphens
         for ($i = 0, $l = $this->leftmin, $wl = mb_strlen($word); $l < ($wl - $this->rightmin); $l++) {
             if (isset($hyphenateWord[$l]) && $hyphenateWord[$l] % 2 !== 0) {
                 array_splice($matchWordChars, $l + $i + 1, 0, $this->hyphen);
